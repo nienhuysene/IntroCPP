9eb69eef1fc7a76ebc681d24e2dbb8519aed6507
Elias Nienhuysen <Elias@MacBook-Pro.local>
2024-12-11 14:49:26 +0100

    New file schuld.cpp added

--- schuld.cpp
+++ schuld.cpp
@@ -10,4 +10,20 @@ int main()
 
     //Enthält der Satz das Wort "Schuld"?
     bool schuld = sentence.find("Schule")!=std::string::npos;
+    if(schuld)
+    {
+        std::cout<<"ja, Schuld"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"nein, keine Schuld. Aber warum?"<<std::endl;
+    }
+
+    //Schreibe alles ab "aus..."
+    std::string Substring1 = sentence.substr(sentence.find("aus"));     //Findet Position von "aus" und Beginnt von dieser Position neuen Substring
+    std::cout<<Substring1<<std::endl;
+
+    //Schreibe alles vor "aus"
+    std::string Substring2 = sentence.substr(0,sentence.find("aus"));
+    std::cout<<Substring2<<std::endl;
 }

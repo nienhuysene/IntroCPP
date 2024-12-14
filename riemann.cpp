f2f592a1d84a9a1f3fe25398efb1daf280b542d8
Elias Nienhuysen <Elias@MacBook-Pro.local>
2024-12-14 10:18:03 +0100

    Riemann integral added

---
+++ riemann.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main()
+{
+    float x=0;
+    float f = pow(x,2)+10;
+
+    //Untersumme
+    for(x = 0;x<=8;)
+    {
+        
+    }
+    //Obersumme
+
+    //Wert des Integrals
+
+
+}
